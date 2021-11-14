f0dfbe8694d8db3658d783340da86d33b15101d9
jaeseong98 <jaeseong2418@gmail.com>
2021-11-14 18:36:18 +0900

    X_train_1_3(all)

--- IPO/Module/.ipynb_checkpoints/Preprocessing-checkpoint.py
+++ IPO/Module/.ipynb_checkpoints/Preprocessing-checkpoint.py
@@ -28,7 +28,7 @@ def ipo_processing(ipo):
     ## 데이터 값 전부 없는 경우 삭제
     delete_item = ["성융광전투자유한공사","리드","케이엠에이치신라레저",
                    "펩트론","파마리서치","신텍","로보티즈","원바이오젠",
-                   "코퍼스코리아"] ## 추가필요
+                   "코퍼스코리아","네프로아이티","수젠텍"] ## 추가필요
     ipo = ipo.drop(delete_item, axis=0)
     
     return ipo
@@ -138,7 +138,6 @@ def MatchItem(IPOcoms, FeatureDf, TargetDf, indexer, finding):
     return FeatureDf
 
 
-
 # +
 def MatchItem_month(IPOcoms, FeatureDf, TargetDf, indexer, finding , month_index , month ):
     """
@@ -219,3 +218,219 @@ test = MatchItem_interest_1_3(IPOcoms, test, market,\
 
 test.to_csv("X_train_1_3.csv",encoding = 'euc-kr')
 ## 위의 트레이딩 + market변수 추가
+
+test = X_train_1_3.copy()
+
+
+def MatchItem_rotation_trading(IPOcoms, FeatureDf, TargetDf, indexer, finding_1,finding_2, name, num):
+    """
+    트레이딩 기준 시가총액 회전율 넣어주는 공식
+    """
+    FeatureDf[name] = np.nan
+    for i in IPOcoms:
+        IPOday = FeatureDf.loc[i,indexer]
+        month = parse(IPOday).date() + relativedelta(months = num)
+        month = str(month)
+        ## 거래대금
+        money = TargetDf.loc[(i,finding_1),IPOday:month].sum(axis=1)[0]
+        ## 시가총액
+        mean_all = TargetDf.loc[(i,finding_2),IPOday:month].mean(axis=1)[0]
+        ## 회전율
+        rotation = (money/mean_all)*100
+        FeatureDf.loc[i,name] = rotation
+    return FeatureDf
+
+
+
+test = MatchItem_rotation_trading(IPOcoms,test,trade,'상장일','거래대금',\
+                                  '시가총액','1개월 시가총액회전율',1)
+test = MatchItem_rotation_trading(IPOcoms,test,trade,'상장일','거래대금',\
+                 '시가총액','3개월 시가총액회전율',3)
+
+
+def MatchItem_rotation_market(IPOcoms, FeatureDf, TargetDf, indexer, finding_1,finding_2, name, num):
+    """
+    시장 기준 시가총액 회전율 넣어주는 공식
+    """
+    FeatureDf[name] = np.nan
+    for i in IPOcoms:
+        IPOday = FeatureDf.loc[i,indexer]
+        month = parse(IPOday).date() + relativedelta(months = num)
+        month = str(month)
+        ## 거래대금
+        money = TargetDf.loc[finding_1,IPOday:month].sum()
+        ## 시가총액
+        mean_all = TargetDf.loc[finding_2,IPOday:month].mean()
+        ## 회전율
+        rotation = (money/(mean_all*1000000))*100
+        FeatureDf.loc[i,name] = rotation
+    return FeatureDf
+
+
+test = MatchItem_rotation_market(IPOcoms,test,market_rotation,'상장일','거래대금',\
+                             '시가총액','1개월 시장 회전율',1)
+test = MatchItem_rotation_market(IPOcoms,test,market_rotation,'상장일','거래대금',\
+                             '시가총액','3개월 시장 회전율',3)
+
+
+# +
+def MatchItem_mean(IPOcoms, FeatureDf, TargetDf, indexer, finding , name, num):
+    """
+    트레이딩 변수들 1,3개월 비중 평균내는 공식
+    """
+    FeatureDf[name] = np.nan
+    for i in IPOcoms:
+        IPOday = FeatureDf.loc[i,indexer]
+        month = parse(IPOday).date() + relativedelta(months = num)
+        month = str(month)
+        ## 변수 평균
+        mean = TargetDf.loc[(i,finding),IPOday:month].mean(axis=1)[0]
+        FeatureDf.loc[i,name] = mean
+    return FeatureDf
+
+
+# -
+
+## 변수 넣어주기
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','거래량회전율',\
+                      '1개월 평균 거래량회전율',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','거래량회전율',\
+                      '3개월 평균 거래량회전율',3)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','일중변동률',\
+                      '1개월 평균 일중변동률',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','일중변동률',\
+                      '3개월 평균 일중변동률',3)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','개인 매도수량 비중(일간)',\
+                      '1개월 평균 개인 매도수량 비중(일간)',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','개인 매도수량 비중(일간)',\
+                      '3개월 평균 개인 매도수량 비중(일간)',3)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','개인 매수수량 비중(일간)',\
+                      '1개월 평균 개인 매수수량 비중(일간)',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','개인 매수수량 비중(일간)',\
+                      '3개월 평균 개인 매수수량 비중(일간)',3)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','기관 매수수량 비중(일간)',\
+                      '1개월 평균 기관 매수수량 비중(일간)',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','기관 매수수량 비중(일간)',\
+                      '3개월 평균 기관 매수수량 비중(일간)',3)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','기관 매도수량 비중(일간)',\
+                      '1개월 평균 기관 매도수량 비중(일간)',1)
+test = MatchItem_mean(IPOcoms,test,trade,'상장일','기관 매도수량 비중(일간)',\
+                      '3개월 평균 기관 매도수량 비중(일간)',3)
+test['IPO시가총액'] = test['공모가(원)']*test['상장주식수']
+
+# > 변수 리스트로 설정해서 위에 코드를 간단하게 하는 함수 만들기
+
+# ## 변수 위치 재설정
+
+test = test[['상장유형',
+ '공모가(원)',
+ '상장일',
+ '상장주식수',
+ 'IPO시가총액',
+ '매출총이익률',
+ '영업이익률',
+ 'ROE(지배)',
+ '매출액증가율(YoY)',
+ '영업이익증가율(YoY)',
+ '매출액증가율(YoY)-before',
+ '영업이익증가율(YoY)-before',
+ '유동비율',
+ '당좌비율',
+ '부채비율',
+ '이자보상배율',
+ '매출채권회전율',
+ '재고자산회전율',
+ '매출액',
+ '세전계속사업이익',
+ '당기순이익(지배)',
+ '자본총계(지배)',
+ 'EBITDA2',
+ '순부채',
+ 'IPO_PER',
+ 'IPO_PBR',
+ 'IPO_EV/EBIDTA',
+ 'IPO_PSR',
+ '1개월 시가총액회전율',
+ '3개월 시가총액회전율',
+ '1개월 평균 거래량회전율',
+ '3개월 평균 거래량회전율',
+ '1개월 평균 일중변동률',
+ '3개월 평균 일중변동률',
+ '1개월 평균 개인 매도수량 비중(일간)',
+ '3개월 평균 개인 매도수량 비중(일간)',
+ '1개월 평균 개인 매수수량 비중(일간)',
+ '3개월 평균 개인 매수수량 비중(일간)',
+ '1개월 평균 기관 매수수량 비중(일간)',
+ '3개월 평균 기관 매수수량 비중(일간)',
+ '1개월 평균 기관 매도수량 비중(일간)',
+ '3개월 평균 기관 매도수량 비중(일간)',
+ '시장수익률',
+'1개월 시장 회전율',
+ '3개월 시장 회전율',
+ 'M2 증감률 ',
+ 'MMF 증감률 ',
+ '고객예탁금 증감률 ',
+ '국고1년시장금리%p',
+ '국고3년시장금리%p',
+ '국고5년시장금리%p']]
+
+## 멀티인덱스 설정
+test.columns = [['IPO']*5 + ['Finance']*23 + ['Trading']*14 + ['Market']*9 ,\
+['IPO지표']*5 + ['수익성']*3 + ['성장성']*4 + ['유동성']*2 + ['안정성']*2 + \
+['활동성']*2 +['규모']*2 + ['삭제할듯']*4 + ['가격결정자 정보']*4 + ['유동지표'] \
+*6 +['매매비중정보']*8 + ['코스닥정보']*3 + ['유동성']*3 +['금리']*3 ,\
+['상장유형',
+ '공모가(원)',
+ '상장일',
+ '상장주식수',
+ 'IPO시가총액',
+ '매출총이익률',
+ '영업이익률',
+ 'ROE(지배)',
+ '매출액증가율(YoY)',
+ '영업이익증가율(YoY)',
+ '매출액증가율(YoY)-before',
+ '영업이익증가율(YoY)-before',
+ '유동비율',
+ '당좌비율',
+ '부채비율',
+ '이자보상배율',
+ '매출채권회전율',
+ '재고자산회전율',
+ '매출액',
+ '세전계속사업이익',
+ '당기순이익(지배)',
+ '자본총계(지배)',
+ 'EBITDA2',
+ '순부채',
+ 'IPO_PER',
+ 'IPO_PBR',
+ 'IPO_EV/EBIDTA',
+ 'IPO_PSR',
+ '1개월 시가총액회전율',
+ '3개월 시가총액회전율',
+ '1개월 평균 거래량회전율',
+ '3개월 평균 거래량회전율',
+ '1개월 평균 일중변동률',
+ '3개월 평균 일중변동률',
+ '1개월 평균 개인 매도수량 비중(일간)',
+ '3개월 평균 개인 매도수량 비중(일간)',
+ '1개월 평균 개인 매수수량 비중(일간)',
+ '3개월 평균 개인 매수수량 비중(일간)',
+ '1개월 평균 기관 매수수량 비중(일간)',
+ '3개월 평균 기관 매수수량 비중(일간)',
+ '1개월 평균 기관 매도수량 비중(일간)',
+ '3개월 평균 기관 매도수량 비중(일간)',
+ '시장수익률',
+'1개월 시장 회전율',
+ '3개월 시장 회전율',
+ 'M2 증감률 ',
+ 'MMF 증감률 ',
+ '고객예탁금 증감률 ',
+ '국고1년시장금리%p',
+ '국고3년시장금리%p',
+ '국고5년시장금리%p']]
+
+# > 엑셀 불러오면 멀티인덱스 풀리기에 불러온 뒤 멀티인덱스 편하게 설정하는 방법 찾기
+
+test.to_csv("X_train_1_3(all).csv", encoding = 'euc-kr')

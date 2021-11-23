706333ae64b821a44ad767200632cd43e89224a9
hyunyun <70521714+hyunyunV@users.noreply.github.com>
2021-11-23 21:59:22 +0900

    WS add Fit w/ Res func

---
+++ IPO/Module/Stat.py
@@ -0,0 +1,38 @@
+from sklearn.metrics import *
+import copy
+import pandas as pd
+import numpy as np
+
+def Result(clf = None, X_train, X_test, y_train, y_test):
+    # 반복문을 돌면서 모형이 지속적으로 fit되는 것을 방지하기 위해서 model의 deepcopy(?)가 필요함
+    # 마찬가지로 하이퍼 파라미터를 맞춘 clf를 밖에서 선언하고 가져와야함
+    clf = RandomForestClassifier() # clf = copy.deepcopy(clf)
+    
+    clf.fit(X_train , y_train)
+    train_pred = clf.predict(X_train)
+    test_pred = clf.predict(X_test)
+    
+    # 성과 평가
+    train_res = clf_eval(y_train, train_pred)
+    test_res = clf_eval(y_test,test_pred)
+    res = train_res + test_res
+    
+    #feature_importance
+    feature_importance = clf.feature_importances_
+    
+    D = {"Score":res, "Feature":feature_importance, "y_pred":test_pred}
+    return D
+
+
+def get_clf_eval(y_test, pred=None, pred_proba=None): 
+    confusion = confusion_matrix(y_test, pred) 
+    accuracy = accuracy_score(y_test, pred) 
+    precision = precision_score(y_test, pred , pos_label = 'positive', average= 'micro') 
+    recall = recall_score(y_test, pred , pos_label = 'positive', average= 'micro') 
+    f1 = f1_score(y_test, pred,pos_label = 'positive', average= 'micro') 
+
+    
+    print('Confusion Matrix') 
+    print(confusion) 
+    print('정확도(accuracy): {0:.4f}, 정밀도(precision): {1:.4f}, 재현율(recall): {2:.4f}, f1_score: {3:.4f}'.format(accuracy, precision, recall, f1))
+    return [accuracy, precision, recall, f1]
